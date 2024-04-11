6df9b07d229cf82fd14d3073619d090cc345cb5e
Kevin <21037571@studentmail.ul.ie>
2024-04-11 21:14:41 +0100

    first edition of profile :3

---
+++ app/src/main/java/com/sam/scenique_app/ui/profile/ProfileFragment.java
@@ -0,0 +1,38 @@
+package com.sam.scenique_app.ui.profile;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.sam.scenique_app.R;
+
+public class ProfileFragment extends Fragment {
+
+    private ProfileViewModel mViewModel;
+
+    public static ProfileFragment newInstance() {
+        return new ProfileFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(ProfileViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/sam/scenique_app/ui/profile/ProfileViewModel.java
@@ -0,0 +1,7 @@
+package com.sam.scenique_app.ui.profile;
+
+import androidx.lifecycle.ViewModel;
+
+public class ProfileViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
