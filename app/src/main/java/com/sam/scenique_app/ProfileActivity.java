9dc68fa1df2afad63f52ec721fc1a1c6cda234ee
Kevin <21037571@studentmail.ul.ie>
2024-05-07 01:32:17 +0100

    Commit Commit Commit

--- app/src/main/java/com/sam/scenique_app/ProfileActivity.java
+++ app/src/main/java/com/sam/scenique_app/ProfileActivity.java
@@ -2,11 +2,14 @@ package com.sam.scenique_app;
 
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private TextView profileName;
+    private TextView profileDescription;
     private String userName;
     private ImageView profilePhoto;
     private float reviewCount;
@@ -14,10 +17,14 @@ public class ProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_profile);
+        getSupportActionBar().setTitle("Profile");
     }
 
     private void setPFPImage(){
+
+    }
+    private void showReviews(){
 
     }
 }
