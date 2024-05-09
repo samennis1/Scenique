0a8f0da30a8d07a8ee3643b029960019f67dbebf
mattodoherty <20266634@studentmail.ul.ie>
2024-05-09 21:44:21 +0100

    display user name in settings, attempt to have about us and faq page accessible

---
+++ app/src/main/java/com/sam/scenique_app/ui/aboutus/AboutUsFragment.java
@@ -0,0 +1,31 @@
+//package com.sam.scenique_app.ui.aboutus;
+//
+//
+//
+//import android.app.AlertDialog;
+//
+//import android.widget.LinearLayout;
+//
+//
+//
+//public class AboutUsFragment {
+//
+////    AlertDialog.Builder builder = new AlertDialog.Builder(context);
+////        builder.setTitle("About Us");
+//
+//
+//    LinearLayout layout = new LinearLayout(context);
+//        layout.setLayoutParams(new LinearLayout.LayoutParams(
+//    LinearLayout.LayoutParams.MATCH_PARENT,
+//    LinearLayout.LayoutParams.WRAP_CONTENT
+//        ));
+//        layout.setOrientation(LinearLayout.VERTICAL);
+//        layout.addView(image);
+//
+//        builder.setView(layout);
+//
+//    AlertDialog dialog = builder.create();
+//        dialog.show();
+//}
+//
+//}
