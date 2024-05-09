31a2a740112205bc4ce450c3f8be04ca6eb5be31
mattodoherty <20266634@studentmail.ul.ie>
2024-05-09 05:33:12 +0100

    Added FAQs and About Us

--- app/src/main/java/com/sam/scenique_app/AboutUsActivity.java
+++ app/src/main/java/com/sam/scenique_app/AboutUsActivity.java
@@ -1,4 +1,29 @@
 package com.sam.scenique_app;
 
-public class AboutUsActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.appcompat.widget.Toolbar;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+
+        Toolbar toolbar = findViewById(R.id.about_us_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
